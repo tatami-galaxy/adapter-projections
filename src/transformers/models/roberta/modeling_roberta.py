448f190a8332e0ce27b6c39326268ca2c8b10f56
ujan <ujandev@gmail.com>
2022-07-16 00:41:20 +0530

    mlm

--- src/transformers/models/roberta/modeling_roberta.py
+++ src/transformers/models/roberta/modeling_roberta.py
@@ -1202,6 +1202,105 @@ class RobertaForMaskedLM(BertModelWithHeadsAdaptersMixin, RobertaPreTrainedModel
         )
 
 
+    def activate_embedding_projection(self):
+        if not self.roberta.encoder.embedding_projection_flag:
+            self.roberta.encoder.embedding_projection_flag = True
+
+    def activate_layer_projections(self, layers: list):
+        for layer_i in layers:
+            if not self.roberta.encoder.layer[layer_i].projection_flag:
+                self.roberta.encoder.layer[layer_i].projection_flag = True
+
+
+    def disable_embedding_projection(self):
+        if self.roberta.encoder.embedding_projection_flag:
+            elf.roberta.encoder.embedding_projection_flag = False
+
+    def disable_layer_projections(self, layers: list):
+        for layer_i in layers:
+            if self.roberta.encoder.layer[layer_i].projection_flag:
+                self.roberta.encoder.layer[layer_i].projection_flag = False
+
+
+    def load_projections(self, lang_list: list, subspace_dir: str):
+        if subspace_dir[-1] != '/': subspace_dir += '/'
+
+        variance_accounted = 0.9
+        num_layers = self.roberta.config.num_hidden_layers
+        dim_size = self.roberta.config.hidden_size
+
+        # compute projections
+        projection_dict = {}
+        means_a_dict = {}
+        means_b_dict = {}
+
+        for lang in lang_list:
+
+            projections = []
+            means_a = []
+            means_b = []
+
+            for layer_i in range(self.config.num_hidden_layers+1):
+                mean_a = np.load(subspace_dir+self.src_lang+'_layer'+str(layer_i)+'_mean.npy') # change for other projections
+                mean_b = np.load(subspace_dir+self.src_lang+'_layer'+str(layer_i)+'_mean.npy') # change for other projections
+
+                means_a.append(mean_a)
+                means_b.append(mean_b)
+
+                s = np.load(subspace_dir+lang+'_layer'+str(layer_i)+'_s.npy')
+                vh = np.load(subspace_dir+lang+'_layer'+str(layer_i)+'_vh.npy')
+                subspace_m = np.load(subspace_dir+lang+'_layer'+str(layer_i)+'_mean.npy')
+
+                v = np.transpose(vh) # columns of V form the desired orthonormal basis
+
+                subspace_dim = 0
+                s_squared = np.square(s)
+                total_variance = np.sum(s_squared) # Proportional to total variance.
+                cutoff_variance = variance_accounted * total_variance
+                curr_variance = 0.0
+                for i in range(s.shape[-1]):
+                    curr_variance += s_squared[i]
+                    if curr_variance >= cutoff_variance:
+                        subspace_dim = i+1
+                        break
+                # Projection matrix: convert into basis (excluding some dimensions), then
+                # convert back into standard basis.
+                v = v[:, :subspace_dim]
+                projection_matrix = np.matmul(v, np.transpose(v))
+                projections.append(projection_matrix)
+
+                projection_dict[lang] = projections
+                means_a_dict[lang] = means_a
+                means_b_dict[lang] = means_b
+
+        self.set_layer_projections(projection_dict, lang_list, means_a_dict, means_b_dict)
+
+
+    def set_layer_projections(self, projection_dict, lang_list, means_a_dict, means_b_dict):
+        for lang in lang_list:
+            projection, projection_shift = self.compute_projection(projection_dict, means_a_dict, means_b_dict, lang, 0) # 0 for embedding layer projections
+            self.roberta.encoder.embedding_projections[lang] = projection #copy.deepcopy(projection)
+            self.roberta.encoder.embedding_projections_shifts[lang] = projection_shift
+            # add shifts here
+
+        for lang in lang_list:
+            for layer_i in range(1, self.config.num_hidden_layers+1):
+                projection, projection_shift = self.compute_projection(projection_dict, means_a_dict, means_b_dict, lang, layer_i)
+                self.roberta.encoder.layer[layer_i-1].layer_projections[lang] = projection
+                self.roberta.encoder.layer[layer_i-1].layer_projections_shifts[lang] = projection_shift
+                # add shifts here
+
+
+    def compute_projection(self, projection_dict, means_a_dict, means_b_dict, lang, layer_i):
+        dim_size = self.config.hidden_size
+        projection = torch.tensor(projection_dict[lang][layer_i]).float() 
+        mean_a = torch.tensor(means_a_dict[lang][layer_i]).float()
+        mean_b = torch.tensor(means_b_dict[lang][layer_i]).float()
+        projection_shift = mean_b - torch.matmul(projection, mean_a)
+        projection_shift = projection_shift.reshape(1, 1, dim_size)
+        return projection, projection_shift
+
+
 
 class RobertaLMHead(nn.Module):
     """Roberta Head for masked language modeling."""
